e2c57003798d5d33cba31d06308e2b4b116491f7
Hamza11067 <hamzakhalid1067@gmail.com>
2025-07-24 09:02:35 +0500

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use("/", (req, res)=> {
-    res.send("Hello, world! from DevTinder! This is the main page.");
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Hamza", lastname: "Khalid" });
 });
 
-app.use("/test",(req,res) => {
-    res.send("Hello, world! from DevTinder test!");
-})
+app.post("/user", (req, res) => {
+  console.log("User data received:");
+  res.send({ message: "User created successfully" });
+});
+
+app.put("/user", (req, res) => {
+  console.log("User data updated");
+  res.send({ message: "User updated successfully" });
+});
+
+app.patch("/user", (req, res) => {
+  console.log("User data updated");
+  res.send({ message: "User updated successfully" });
+});
+
+app.delete("/user", (req, res) => {
+  console.log("User deleted");
+  res.send({ message: "User deleted successfully" });
+});
+
+// Order of routes matters, so ensure the test route is defined before the main route
 
 app.listen(4000, () => {
-  console.log('Server is running on port 4000');
+  console.log("Server is running on port 4000");
 });
