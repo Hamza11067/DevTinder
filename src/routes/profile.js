08c0fac88f063319e62de5a3404d3987a09bdb2a
Hamza11067 <hamzakhalid1067@gmail.com>
2025-08-27 11:22:07 +0500

    Created profile edit API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,17 +1,39 @@
-const express = require('express');
+const express = require("express");
 const profileRouter = express.Router();
-const {userAuth} = require('../middlewares/auth')
+const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
-
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
-
   } catch (error) {
     console.error("Error fetching profile:", error);
-    res.status(500).json({ message: "Error fetching profile", error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error fetching profile", error: error.message });
   }
 });
 
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    const isDataValid = validateEditProfileData(req);
+    if (!isDataValid) {
+      return res
+        .status(400)
+        .json({ message: "Invalid fields in request body" });
+    }
+    const user = req.user;
+    Object.keys(req.body).forEach((key) => {
+      user[key] = req.body[key];
+    });
+    await user.save();
+    res.send(`${user.firstName}, your profile updated successfully`);
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    res
+      .status(500)
+      .json({ message: "Error updating profile", error: error.message });
+  }
+});
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,17 +1,40 @@
-const validator = require('validator');
+const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const { firstName, email, password } = req.body;
+  const { firstName, email, password } = req.body;
 
-    if (!firstName || firstName.length < 3 || firstName.length > 50) {
-        throw new Error("First name must be between 3 and 50 characters");
-    }else if (!email || !validator.isEmail(email)) {
-        throw new Error("Invalid email format");
-    } else if (!password || password.length < 8 || !validator.isStrongPassword(password)) {
-        throw new Error("Password must be at least 8 characters long and strong");
-    }
-}
+  if (!firstName || firstName.length < 3 || firstName.length > 50) {
+    throw new Error("First name must be between 3 and 50 characters");
+  } else if (!email || !validator.isEmail(email)) {
+    throw new Error("Invalid email format");
+  } else if (
+    !password ||
+    password.length < 8 ||
+    !validator.isStrongPassword(password)
+  ) {
+    throw new Error("Password must be at least 8 characters long and strong");
+  }
+};
+
+const validateEditProfileData = (req) => {
+  const allowedFields = [
+    "firstName",
+    "lastName",
+    "about",
+    "skills",
+    "age",
+    "gender",
+    "photoURL",
+  ];
+
+  const isAllowed = Object.keys(req.body).every((field) =>
+    allowedFields.includes(field)
+  );
+
+  return isAllowed;
+};
 
 module.exports = {
-    validateSignUpData
-}
+  validateSignUpData,
+  validateEditProfileData,
+};
